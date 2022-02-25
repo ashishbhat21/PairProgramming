8425cb34a04dc3c9a3c77342b40a0ec5c91624d6
Ashwin Rachha <ashwin.rachha@gmail.com>
2022-02-25 15:22:52 -0500

    Pushed Roman to Integer

---
+++ main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        singles = {'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
+
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        result = 0
+
+        for c in s:
+
+            result += singles[c]
+
+        return result
