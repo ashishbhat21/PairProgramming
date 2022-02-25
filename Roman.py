7d6ef584fa109d3520a2467307d552705e07ddf2
AshwinRachha <ashwin.rachha@gmail.com>
2022-02-25 16:02:39 -0500

    Test

--- Roman.py
+++ Roman.py
@@ -1,14 +1,22 @@
-# the function converts an integer to a roman numeral
-# param num - the number to be converted to a roman numeral
-# return - string conataining the roman numeral
-# author - Ashish Bhat
-# text editor - visual studio code
+# the function converts a roman numeral to integer
+# parameters : s -> signifying roman numeral in string form
+# return - integer value of roman numeral
+# author - Ashwin Rachha
+# text editor - atom
 
-def intToRoman(self, num: int) -> str:
-        d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
-        res = ''
-        for k in d:
-            while num >= k:
-                res += d[k]
-                num -= k
-        return res
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        singles = {'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
+
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        result = 0
+
+        for c in s:
+
+            result += singles[c]
+
+        return result
